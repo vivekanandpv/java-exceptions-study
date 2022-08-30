6aad4846d32e71c834f094d50a9c50fa488d8953
Vivekanand P V <vivekanandpv@outlook.com>
2022-08-30 22:37:17 +0530

    done

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,14 @@ public class Main {
             throw new RecordNotFoundException("Could not find the record!");
         } catch (RecordNotFoundException rnf) {
             System.out.println(rnf.getMessage());
+        } finally {
+            System.out.println("This runs anyway!");
+
+            //  finally block is optional
+            //  there can only be one finally block per try block
+            //  finally must come after all catch blocks
+            //  try must accompany catch or finally or both
+            //  use finally to release the system resources such as file handles
         }
     }
 }
