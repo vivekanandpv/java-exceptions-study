5feeeeba27037417ed10fdb580a720c730e27d1b
Vivekanand P V <vivekanandpv@outlook.com>
2022-08-30 21:52:51 +0530

    done

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,19 @@
 
 public class Main {
     public static void main(String[] args) {
+        //  Exceptions are thrown because of logical mistakes
+        //  or underlying system conditions or due to unmet
+        //  requirements of APIs
 
+        String message = null;
+
+        String upper = message.toUpperCase();   //  NullPointerException is thrown here
+
+        //  This exception is unhandled and hence reaches the JVM
+        //  wherein the runtime terminates the program with an error
+        //  (a non-zero exit)
+
+        //  An exception is an object that encapsulates message related
+        //  to the exception and also the call stack information.
     }
 }
