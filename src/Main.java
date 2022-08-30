5b35a945c70ece76ab3123e99573d7fa575fe1cb
Vivekanand P V <vivekanandpv@outlook.com>
2022-08-30 22:42:11 +0530

    done

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,33 @@
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            throw new RecordNotFoundException("Could not find the record!");
-        } catch (RecordNotFoundException rnf) {
-            System.out.println(rnf.getMessage());
-        } finally {
-            System.out.println("This runs anyway!");
+        Path filePath = Path.of("src/sample.txt");
+        Reader reader = null;
 
-            //  finally block is optional
-            //  there can only be one finally block per try block
-            //  finally must come after all catch blocks
-            //  try must accompany catch or finally or both
-            //  use finally to release the system resources such as file handles
+        try {
+            reader = new BufferedReader(new FileReader(filePath.toFile()));
+
+            int ch;
+
+            while ((ch = reader.read()) != -1) {
+                System.out.println((char) ch);
+            }
+
+            System.out.println("Done!");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    System.out.println("Oops! we cannot do anything now...");
+                }
+            }
         }
     }
 }
