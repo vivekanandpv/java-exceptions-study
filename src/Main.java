722c563e8a9bd741a7bfdc365c0c152a00b2c4ed
Vivekanand P V <vivekanandpv@outlook.com>
2022-08-30 22:08:20 +0530

    done

--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,9 @@
 public class Main {
     public static void main(String[] args) {
         try {
-            foo();
+            throw new NullPointerException();   //  Only Throwable objects can be thrown
         } catch (NullPointerException npe) {
             System.out.println("main: Oops!");
         }
-
-    }
-
-    public static void foo() {
-        try {
-            bar();
-        } catch (NullPointerException npe) {
-            System.out.println("foo: Oops!");
-        }
-    }
-
-    public static void bar() {
-        String name = null;
-        try {
-            System.out.println(name.toUpperCase());
-        } catch (NullPointerException npe) {
-            System.out.println("bar: Oops!");
-        }
-
     }
 }
