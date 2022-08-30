6b85413bf7cc4d8a2db1675a474d7d1305c8a9cf
Vivekanand P V <vivekanandpv@outlook.com>
2022-08-30 22:47:10 +0530

    done

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,19 @@ import java.io.*;
 import java.nio.file.Path;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        //  Starting with Java 7, a better approach called try-with-resource has been introduced
+        //  This works with the types that implement AutoClosable interface
+        //  Reader is such a type
+
         Path filePath = Path.of("src/sample.txt");
-        Reader reader = null;
 
-        try {
-            reader = new BufferedReader(new FileReader(filePath.toFile()));
+        //  reader is a resource and this construct makes sure that the close on the resource is called
+        //  multiple resources must be semicolon separated
 
+        //  finally is implicit here
+        //  so catch blocks are optional (throws claus is added to this method)
+        try (Reader reader = new BufferedReader(new FileReader(filePath.toFile()))) {
             int ch;
 
             while ((ch = reader.read()) != -1) {
@@ -16,18 +22,6 @@ public class Main {
             }
 
             System.out.println("Done!");
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    System.out.println("Oops! we cannot do anything now...");
-                }
-            }
         }
     }
 }
