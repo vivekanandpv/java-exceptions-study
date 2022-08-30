dc781fb8ab6d75620d5f47d421f95b06165445d7
Vivekanand P V <vivekanandpv@outlook.com>
2022-08-30 22:30:07 +0530

    done

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,36 @@
-
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            throw new NullPointerException();   //  Only Throwable objects can be thrown
-        } catch (RuntimeException re) {
-            System.out.println("main: RuntimeException");
-        }
+        //  Exception and its subclasses except the hierarchy of RuntimeException
+        //  are known as checked exceptions.
 
-        //  Since NullPointerException is a subclass of RuntimeException,
-        //  catch block of RuntimeException consumes the NullPointerException
-        //  provided such an exception is not handled already
+        //  Compiler enforces the handling of these checked exceptions as either
+        //  explicit handling with try-catch
+        //  or, mentioning the exception in the throws clause of the method
+        //  If the called method has a throws clause of a checked exception
+        //  then the caller must either handle that or in turn mention the exception
+        //  in its throws clause. If an unchecked exception is mentioned in the throws clause,
+        //  it has no binding on the caller, however. If multiple checked exceptions are possible,
+        //  they must be comma separated in the throws clause. Handling some exceptions and mentioning
+        //  others in the throws clause is possible as well.
+
+
+        //  explicit handling
+        try {
+            foo();
+        } catch (IOException e) {
+            System.out.println("oops!");
+
+            //  If an unchecked exception is thrown after catching the checked
+            //  exception, it is called rethrow or exception masking
+
+            //  example code:
+            //  throw new RuntimeException(e);
+        }
+    }
+
+    public static void foo() throws IOException {
+        throw new IOException();
     }
 }
